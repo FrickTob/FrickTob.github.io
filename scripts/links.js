7b6806f5e47eb435856e158b56259826ea5e482b
T-Frick <toby.frick@hotmail.com>
2023-03-23 19:26:45 +0100

    Updated Featured Projects

--- scripts/links.js
+++ scripts/links.js
@@ -4,5 +4,5 @@ const project3 = document.getElementById('projectImg3')
 const project4 = document.getElementById('projectImg4')
 project1.onclick = () => location.href = 'https://github.com/FrickTob/Chess-Vision'
 project2.onclick = () => location.href = 'https://github.com/FrickTob/HueDoku-Android-App'
-project3.onclick = () => location.href = 'https://github.com/FrickTob/HueDoku-Android-App'
-project4.onclick = () => location.href = 'https://github.com/FrickTob/HueDoku-Android-App'
+project3.onclick = () => location.href = 'https://github.com/FrickTob/Personal-Website'
+project4.onclick = () => location.href = 'https://github.com/FrickTob/Poker-Helper-Application'
