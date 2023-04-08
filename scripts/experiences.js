7843a7a039741349ea0626bf49526a49d20988f0
T-Frick <toby.frick@hotmail.com>
2023-04-08 10:44:47 +0200

    Added collapsing tabs to experience section

---
+++ scripts/experiences.js
@@ -0,0 +1,23 @@
+var abroadButton = document.querySelector('.abroadButton');
+var tutorButton = document.querySelector('.tutorButton');
+var internButton = document.querySelector('.internButton');
+var abroadBox = document.querySelector('.abroadBox');
+var tutorBox = document.querySelector('.tutorBox');
+var internBox = document.querySelector('.internBox');
+
+tutorButton.addEventListener('click', function() {
+    tutorBox.style.height = '70%';
+    abroadBox.style.height = '0';
+    internBox.style.height = '0';
+});
+
+abroadButton.addEventListener('click', function() {
+    tutorBox.style.height = '0';
+    abroadBox.style.height = '70%';
+    internBox.style.height = '0';
+});
+internButton.addEventListener('click', function() {
+    tutorBox.style.height = '0';
+    abroadBox.style.height = '0';
+    internBox.style.height = '70%';
+});
--- scripts/links.js
+++ scripts/links.js
@@ -2,7 +2,7 @@ const project1 = document.getElementById('projectBox1')
 const project2 = document.getElementById('projectImg2')
 const project3 = document.getElementById('projectImg3')
 const project4 = document.getElementById('projectImg4')
-project1.onclick = () => location.href = 'https://github.com/FrickTob/Chess-Vision'
-project2.onclick = () => location.href = 'https://github.com/FrickTob/HueDoku-Android-App'
-project3.onclick = () => location.href = 'https://github.com/FrickTob/Personal-Website'
-project4.onclick = () => location.href = 'https://github.com/FrickTob/Poker-Helper-Application'
+project1.onclick = () => window.open('https://github.com/FrickTob/Chess-Vision', '_blank').focus()
+project2.onclick = () => window.open('https://github.com/FrickTob/HueDoku-Android-App','_blank').focus()
+project3.onclick = () => window.open('https://github.com/FrickTob/Personal-Website', '_blank').focus()
+project4.onclick = () => window.open('https://github.com/FrickTob/Poker-Helper-Application', '_blank').focus()
