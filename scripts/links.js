ee79a8939a722e9bdee81635cd5c1deb2a150b8d
T-Frick <toby.frick@hotmail.com>
2023-04-20 11:16:17 +0200

    Updated Project Links
    Replaced js with anchor tags so you there is a link preview on hover

--- scripts/links.js
+++
@@ -1,8 +0,0 @@
-const project1 = document.getElementById('projectBox1')
-const project2 = document.getElementById('projectImg2')
-const project3 = document.getElementById('projectImg3')
-const project4 = document.getElementById('projectImg4')
-project1.onclick = () => window.open('https://github.com/FrickTob/Chess-Vision', '_blank').focus()
-project2.onclick = () => window.open('https://github.com/FrickTob/HueDoku-Android-App','_blank').focus()
-project3.onclick = () => window.open('https://github.com/FrickTob/Personal-Website', '_blank').focus()
-project4.onclick = () => window.open('https://github.com/FrickTob/Hold-Em-Helper-Android-App', '_blank').focus()
